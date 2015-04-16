a01f418eeb5b34aebd5d02d5b6581e73ca76ffd7
John Osorio <john@sirius.utp.edu.co>
2015-04-16 10:42:48 -0500

    Se adicionan archivos de entrada en la carpeta inputs y ya se calcula la YBUS de manera correcta

--- utilities/utilities.c
+++ utilities/utilities.c
@@ -2,8 +2,54 @@
 #include "utilities.h"
 #include <string.h>
 
-int calcularYbus(structData *data, double *ybus){
-    ybus = malloc(data->numN*data->numN*sizeof(double));
+int genVector(int *NNP, int initNumber,int finalNumber){
+    int i = initNumber,j=0;
+    for (i = initNumber; i <= finalNumber; i++) {
+        NNP[j] = i;
+        j++;
+    }
+    return 0;
+}
+
+int calcularYbus(structData *data, double *ybusReal, double *ybusImag){
+    zeros(data->numN*data->numN, ybusReal);
+    zeros(data->numN*data->numN, ybusImag);
+    int widthLineas = 6;
+    double N1, N2, ym;
+    double YYReal, YYImag, tap;
+    int k,t;
+    for (k = 0; k < data->numL; k++) {
+        N1 = data->lineas[k*widthLineas+0]-1;
+        N2 = data->lineas[k*widthLineas+1]-1;
+        ym = data->lineas[k*widthLineas+2]*data->lineas[k*widthLineas+2]+\
+             data->lineas[k*widthLineas+3]*data->lineas[k*widthLineas+3];
+        YYReal = data->lineas[k*widthLineas+2]/ym;
+        YYImag = -data->lineas[k*widthLineas+3]/ym;
+
+        tap = 1/data->lineas[k*widthLineas+5];
+
+        ybusReal[(int) (N1*data->numN + N1)] = ybusReal[(int) (N1*data->numN + N1)]\
+                                               + tap*tap*YYReal;
+        ybusReal[(int) (N2*data->numN + N2)] = ybusReal[(int) (N2*data->numN + N2)] \
+                                               + YYReal;
+        ybusReal[(int) (N1*data->numN + N2)] = ybusReal[(int) (N1*data->numN + N2)] \
+                                               - tap*YYReal;
+        ybusReal[(int) (N2*data->numN + N1)] = ybusReal[(int) (N2*data->numN + N1)] \
+                                               - tap*YYReal;
+
+        ybusImag[(int) (N1*data->numN + N1)] = ybusImag[(int) (N1*data->numN + N1)] \
+                                               + tap*tap*YYImag + data->lineas[k*widthLineas+4];
+        ybusImag[(int) (N2*data->numN + N2)] = ybusImag[(int) (N2*data->numN + N2)] \
+                                               + YYImag + data->lineas[k*widthLineas+4];
+        ybusImag[(int) (N1*data->numN + N2)] = ybusImag[(int) (N1*data->numN + N2)] \
+                                               - tap*YYImag;
+        ybusImag[(int) (N2*data->numN + N1)] = ybusImag[(int) (N2*data->numN + N1)] \
+                                               - tap*tap*YYImag;
+    }
+
+
+    return 0;
+
 
 }
 
@@ -65,7 +111,7 @@ int printData(structData *data, int widthLineas, int heightLineas, int widthCarg
 int loadDataFromFile(char *filenameLineas, char *filenameCargas, char *filenameGen, structData *data){
     FILE *datosLineas,*datosGen,*datosCargas;
     int i, j, widthLineas,heightLineas,widthGen, heightGen,widthCargas, heightCargas;
-    widthLineas = 5;
+    widthLineas = 6;
     heightLineas = 186;
     widthGen = 3;
     heightGen = 15;
@@ -108,9 +154,11 @@ int loadDataFromFile(char *filenameLineas, char *filenameCargas, char *filenameG
         printf("Imposible asignar memoria a cargas\n");
         exit(1);
     }
-
-    for (i = 0; i < heightLineas; i++) {
-        fscanf(datosLineas, "%lf,%lf,%lf,%lf,%lf\n",&data->lineas[i*widthLineas],&data->lineas[i*widthLineas+1],&data->lineas[i*widthLineas+2],&data->lineas[i*widthLineas+3],&data->lineas[i*widthLineas+4]);
+    for (i = 0; i < heightLineas; i++) {//Se adiciona el tap constante en 1.0 para este caso
+        fscanf(datosLineas, "%lf,%lf,%lf,%lf,%lf\n",&data->lineas[i*widthLineas],\
+                &data->lineas[i*widthLineas+1],&data->lineas[i*widthLineas+2],\
+                &data->lineas[i*widthLineas+3],&data->lineas[i*widthLineas+4]);
+        data->lineas[i*widthLineas+5] = 1.0;
     }
 
     for (i = 0; i < heightCargas; i++) {
--- utilities/utilities.h
+++ utilities/utilities.h
@@ -13,10 +13,10 @@ typedef struct{
 } structData;
 
 double maxLineas(structData *data, int widthLineas,int heightLineas);
-int calcularYbus(structData *data, double *ybus);
+int calcularYbus(structData *data, double *ybusReal, double * ybusImag);
 int ones(int size,double *Vn);
 int zeros(int size,double *An);
 int printData(structData *data, int widthLineas, int heightLineas, int widthCargas, int heightCargas, int widthGen, int heightGen);
 int loadDataFromFile(char *filenameLineas, char *filenameCargas, char* filenameGen, structData *data);
-
+int genVector(int *NNP, int initNumber, int finalNumber);
 
